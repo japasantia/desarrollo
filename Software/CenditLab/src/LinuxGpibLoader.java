35240285ab1f5a4ebf1497a446ad7b167bd87c2a
Jose Arias <ja.pasantia@gmail.com>
2017-06-15 16:23:05 -0400

    Inicia el desarrollo formal de CenditLab
    - Inicio desarrolo librerias comunicacion (LinuxGpib)

---
+++ Software/CenditLab/src/LinuxGpibLoader.java
@@ -0,0 +1,29 @@
+package ve.com.josearias.jnatest;
+
+import com.sun.jna.Native;
+
+/**
+ * Created by jarias on 15/06/17.
+ */
+public class LinuxGpibLoader
+{
+    private static ILinuxGpib library;
+
+    static
+    {
+        try
+        {
+            library = Native.loadLibrary("gpib", ILinuxGpib.class);
+        }
+        catch (Exception ex)
+        {
+            System.err.println(ex.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public static ILinuxGpib getLibrary()
+    {
+        return library;
+    }
+}
