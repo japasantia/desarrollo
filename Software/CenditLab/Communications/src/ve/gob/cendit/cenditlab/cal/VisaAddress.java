cc506c9a33314cf9b4b18171bc585340842c7b1b
Jose Arias <ja.pasantia@gmail.com>
2017-06-19 22:41:43 -0400

    Trabajo en CenditLab: VisaAddress.java
    - Se busca forma de validar direcciones VISA de manera simple y eficiente

--- Software/CenditLab/Communications/src/ve/gob/cendit/cenditlab/cal/VisaAddress.java
+++ Software/CenditLab/Communications/src/ve/gob/cendit/cenditlab/cal/VisaAddress.java
@@ -91,90 +91,257 @@ public class VisaAddress
         return interfaceType;
     }
 
- 
-
     public static boolean TryParseVisaAddress(String address, VisaAddress visaAddress)
     {
-
+        return false;
     }
 }
 
-class VisaAddressPatterns 
+class VisaAddressFields
 {
-    public final static String VXI_INSTR_PATTERN =
-            "(VXI)(\\d+)?::(\\d+)(::(INSTR))?";
-    public final  static String VXI_MEMACC_PATTERN =
-            "(VXI)(\\d+)?::(MEMACC)";
-    public final static String VXI_BACKPLANE_PATTERN =
-            "(VXI)(\\d+)?(::(\\d+))?::(BACKPLANE)";
-    public final static String VXI_SERVANT_PATTERN =
-            "(VXI)(\\d+)?::(SERVANT)";
-
-    public final static String GPIB_VXI_INSTR_PATTERN =
-            "(GPIB-VXI)(\\d+)?::(\\d+)(::(INSTR))?";
-    public final static String GPIB_VXI_MEMACC =
-            "(GPIB-VXI)(\\d+)?::(MEMACC)";
-    public final static String GPIB_VXI_BACKPLANE =
-            "(GPIB-VXI)(\\d+)?(::(\\d+))?::(BACKPLANE)";
-
-    public final static String GPIB_INSTR_PATTERN =
-            "(GPIB)(\\d+)?::(\\d+)(::(\\d+))?(::(INSTR))?";
-    public final static String GPIB_INTFC_PATTERN =
-            "(GPIB)(\\d+)?::(INTFC)";
-    public final static String GPIB_SERVANT_PATTERN =
-            "(GPIB)(\\d+)?::(SERVANT)";
-
-    public final static String PXI_INSTR_1_PATTERN =
-            "(PXI)(\\d+)?::(\\d+)(::(\\d+))?(::(INSTR))?";
-    public final  static String PXI_INSTR_2_PATTERN =
-            "(PXI)(\\d+)?::((\\d+)-)?(\\d+)(.(\\d+))?(::(INSTR))?";
-    public final static String PXI_MEMACC_PATTERN =
-            "(PXI)(\\d+)?::MEMACC";
-
-    public final static String SERIAL_INSTR_PATTERN =
-            "(ASRL)(\\d)?(::(INSTR))?";
-
-    public final static String TCPIP_INSTR_1_PATTERN =
-            "(TCPIP)(\\d)?::(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(::([^:]*))?(::(INSTR))?";
-    public final static String TCPIP_INSTR_2_PATTERN =
-            "(TCPIP)(\\d)?::(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})::(\\d{1,5})::(SOCKET)";
-
-    public final static String USB_INSTR_PATTERN =
-            "(USB)(\\d+)?::(::([^:]*))::(::([^:]*))::(::([^:]*))(::(\\d+))?(::(INSTR))?";
-    public final  static String USB_INSTR_RAW_PATTERN =
-            "(USB)(\\d+)?::(::([^:]*))::(::([^:]*))::(::([^:]*))(::(\\d+))?(::(RAW))?";
+    public static final int INTERFACE_TYPE = 1;
+    public static final int BOARD_INDEX = 2;
+    public static final int LOGICAL_ADDRESS = 2;
+    public static final int MAINFRAME_LOGICAL_ADDRESS = 3;
+    public static final int RESOURCE_TYPE = 4;
+    public static final int PRIMARY_ADDRESS = 2;
+    public static final int SECONDARY_ADDRESS = 3;
+    public static final int TCPIP_ADDRESS = 2;
+    public static final int TCPIP_PORT = 3;
+    public static final int LAN_DEVICE_NAME = 2;
+    public static final int MANUFACTURER_ID = 2;
+    public static final int MODEL_CODE = 3;
+    public static final int SERIAL_NUMBER = 4;
+    public static final int INTERFACE_NUMBER = 5;
 }
 
-interface IVisaAddressParser
+class VisaAddressFieldNames
+{
+    public static final String INTERFACE = "INTERFACE";
+    public static final String BOARD = "BOARD";
+    public static final String LOGICAL_ADDRESS = "LOGICALADDRESS";
+    public static final String RESOURCE = "RESOURCE";
+    public static final String PRIMARY_ADDRESS = "PRIMARYADDRESS";
+    public static final String SECONDARY_ADDRESS = "SECONDARYADDRESS";
+    public static final String DEVICE = "DEVICE";
+    public static final String FUNCTION = "FUNCTION";
+    public static final String DEVICE_NAME = "DEVICENAME";
+    public static final String PORT = "PORT";
+    public static final String MANUFACTURER_ID = "MANUFACTURERID";
+    public static final String MODEL_CODE = "MODELCODE";
+    public static final String SERIAL_NUMBER = "SERIALNUMBER";
+    public static final String INTERFACE_NUMBER = "INTERFACE_NUMBER";
+}
+
+class VisaAddressPatterns
+{
+    /*Colocar nombre a los grupos regex
+    https://stackoverflow.com/questions/415580/regex-named-groups-in-java
+    "(?<login>\\w+) (?<id>\\d+)"
+    matcher.group("login") ==> TEST
+    */
+
+    public final static String VXI_INSTR =
+            "(?<INTERFACE>VXI)(?<BOARD>\\d+)?::(?<LOGICAL_ADDRESS>\\d+)(::(?<RESOURCE>INSTR))?";
+    public final  static String VXI_MEMACC =
+            "(?<INTERFACE>VXI)(?<BOARD>\\d+)?::(?<RESOURCE>MEMACC)";
+    public final static String VXI_BACKPLANE =
+            "(?<INTERFACE>VXI)(?<BOARD>\\d+)?(::(?<LOGICALADDRESS>\\d+))?::(?<RESOURCE>BACKPLANE)";
+    public final static String VXI_SERVANT =
+            "(?<INTERFACE>VXI)(?<BOARD>\\d+)?::(?<RESOURCE>SERVANT)";
+
+    public final static String GPIB_VXI_INSTR =
+            "(?<INTERFACE>GPIB-VXI)(?<BOARD>\\d+)?::(?<LOGICALADDRESS>\\d+)(?<RESOURCE>::(INSTR))?";
+    public final static String GPIB_VXI_MEMACC =
+            "(?<INTERFACE>GPIB-VXI)(?<BOARD>\\d+)?::(?<RESOURCE>MEMACC)";
+    public final static String GPIB_VXI_BACKPLANE =
+            "(?<INTERFACE>GPIB-VXI)(?<BOARD>\\d+)?(::(?<LOGICALADDRESS>\\d+))?::(?<RESOURCE>BACKPLANE)";
+
+    public final static String GPIB_INSTR =
+            "(?<INTERFACE>GPIB)(?<BOARD>\\d+)?::(?<PRIMARYADDRESS>\\d+)(::(?<SECONDARYADDRESS>\\d+))?(::(?<RESOURCE>INSTR))?";
+    public final static String GPIB_INTFC =
+            "(?<INTERFACE>GPIB)(?<BOARD>\\d+)?::(?<RESOURCE>INTFC)";
+    public final static String GPIB_SERVANT =
+            "(?<INTERFCE>GPIB)(?<BOARD>\\d+)?::(?<RESOURCE>SERVANT)";
+
+    public final static String PXI_INSTR_1 =
+            "(?<INTERFACE>PXI)(?<BOARD>\\d+)?::(?<DEVICE>\\d+)(::(?<FUNCTION>\\d+))?(::(?<RESOURCE>INSTR))?";
+    public final  static String PXI_INSTR_2 =
+            "(?<INTERFACE>PXI)(?<BOARD>\\d+)?::((?<DEVICE>\\d+)-)?(?<FUNCTION>\\d+)(.(\\d+))?(::(?<RESOURCE>INSTR))?";
+    public final static String PXI_MEMACC =
+            "(?<INTERFACE>PXI)(?<BOARD>\\d+)?::(?<RESOURCE>MEMACC)";
+
+    public final static String SERIAL_INSTR =
+            "(?<INTERFACE>ASRL)(?<BOARD>\\d)?(::(?<RESOURCE>INSTR))?";
+
+    public final static String TCPIP_INSTR_1 =
+            "(?<INTERFACE>TCPIP)(?<BOARD>\\d)?::(?<HOSTADDRESS>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(::(?<DEVICENAME>[^:]*))?(::(?<RESOURCE>INSTR))?";
+    public final static String TCPIP_INSTR_2 =
+            "(?<INTERFACE>TCPIP)(?<BOARD>\\d)?::(?<HOSTADDRESS>\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})::(?<PORT>\\d{1,5})::(?<RESOURCE>SOCKET)";
+
+    public final static String USB_INSTR =
+            "(?<INTERFACE>USB)(?<BOARD>\\d+)?::(?<MANUFACTURERID>[^:]*)::(?<MODELCODE>[^:]*)::(?<SERIALNUMBER>[^:]*)(::(?<INTERFACENUMBER>\\d+))?(::(?<RESOURCE>INSTR))?";
+    public final  static String USB_INSTR_RAW =
+            "(?<INTERFACE>USB)(?<BOARD>\\d+)?::(?<MANUFACTURERID>[^:]*)::(?<MODELCODE>[^:]*)::(?<SERIALNUMBER>[^:]*)(::(?<INTERFACENUMBER>\\d+))?(::(?<RESOURCE>RAW))?";
+}
+
+
+class VisaAddressParseFunctions
+{
+    static VisaAddress parseVxiInstrAddress(String address)
+    {
+        Pattern pattern = Pattern.compile(VisaAddressPatterns.GPIB_INSTR);
+        Matcher matcher = pattern.matcher(address);
+
+        try {
+            if (matcher.find()) {
+                String interfaceType = matcher.group(VisaAddressFields.INTERFACE_TYPE);
+                String board = matcher.group(VisaAddressFields.BOARD_INDEX);
+                String logicalAddress = matcher.group(VisaAddressFields.LOGICAL_ADDRESS);
+                String resourceType = matcher.group(VisaAddressFields.RESOURCE_TYPE);
+
+                return new VisaAddress(address);
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+        return null;
+    }
+
+    static Function<String, VisaAddress> vxiInstrParseAddress = a -> parse(a);
+}
+
+interface IVisaAddressParserFunction
 {
     VisaAddress parse(String address);
     boolean tryParse(String address, VisaAddress visaAddress);
 }
 
-class GpibVisaAddressParser implements IVisaAddressParser
+class VisaAddressParseFunction implements Function<String, VisaAddress>
+{
+
+    @Override
+    public VisaAddress apply(String s)
+    {
+        return null;
+    }
+
+    @Override
+    public <V> Function<V, VisaAddress> compose(Function<? super V, ? extends String> before)
+    {
+        return null;
+    }
+
+    @Override
+    public <V> Function<String, V> andThen(Function<? super VisaAddress, ? extends V> after) {
+        return null;
+    }
+}
+
+class PatternParseFunctionPair
+{
+    public String addressPattern;
+    public Function<Matcher, VisaAddress> addressExtractFunction;
+
+    public PatternParseFunctionPair(String addressPattern,
+                                    Function<Matcher, VisaAddress> addressExtractFunction)
+    {
+        this.addressPattern = addressPattern;
+        this.addressExtractFunction = addressExtractFunction;
+    }
+}
+
+
+
+class VisaAddressParser
+{
+    private static String[] patterns;
+    private static String[] fieldNames;
+
+    private static Function<String, VisaAddress>[] addressFieldExtractFunctions;
+
+    static
+    {
+        patterns = new String[]
+        {
+            VisaAddressPatterns.VXI_INSTR,
+            VisaAddressPatterns.VXI_MEMACC,
+            VisaAddressPatterns.VXI_BACKPLANE,
+            VisaAddressPatterns.VXI_SERVANT,
+
+            VisaAddressPatterns.GPIB_VXI_INSTR,
+            VisaAddressPatterns.GPIB_VXI_MEMACC,
+            VisaAddressPatterns.GPIB_VXI_BACKPLANE,
+
+            VisaAddressPatterns.GPIB_INSTR,
+            VisaAddressPatterns.GPIB_INTFC,
+            VisaAddressPatterns.GPIB_SERVANT,
+
+            VisaAddressPatterns.PXI_INSTR_1,
+            VisaAddressPatterns.PXI_INSTR_2,
+            VisaAddressPatterns.PXI_MEMACC,
+
+            VisaAddressPatterns.SERIAL_INSTR,
+
+            VisaAddressPatterns.TCPIP_INSTR_1,
+            VisaAddressPatterns.TCPIP_INSTR_2,
+
+            VisaAddressPatterns.USB_INSTR,
+            VisaAddressPatterns.USB_INSTR_RAW
+        };
+
+        fieldNames = new String[]
+        {
+            VisaAddressFieldNames.INTERFACE,
+            VisaAddressFieldNames.BOARD,
+            VisaAddressFieldNames.LOGICAL_ADDRESS,
+            VisaAddressFieldNames.RESOURCE,
+            VisaAddressFieldNames.PRIMARY_ADDRESS,
+            VisaAddressFieldNames.SECONDARY_ADDRESS,
+            VisaAddressFieldNames.DEVICE,
+            VisaAddressFieldNames.FUNCTION,
+            VisaAddressFieldNames.DEVICE_NAME,
+            VisaAddressFieldNames.PORT,
+            VisaAddressFieldNames.MANUFACTURER_ID,
+            VisaAddressFieldNames.MODEL_CODE,
+            VisaAddressFieldNames.SERIAL_NUMBER,
+            VisaAddressFieldNames.INTERFACE_NUMBER
+        };
+    }
+
+    public void tets()
+    {
+        for(int i = 0; i < patterns.length; ++i)
+        {
+            Function<String, VisaAddress> f = addressFieldExtractFunctions[i];
+            f.apply(patterns[i]);
+        }
+
+        Arrays.stream(patterns).filter(a -> a.matches("\\d+"));
+        addressFieldExtractFunctions[i].apply(patterns[i]);
+    }
+
+
+}
+
+/*class GpibVisaAddressParser implements IVisaAddressParser
 {
     private String address;
     private class PatternParseFunction
     {
         public String pattern;
-        public Function<String, VisaAddress> parseFunction;
+        public Function<Matcher, VisaAddress> parseFunction;
 
         public PatternParseFunction(String p,
-                    Function<String, VisaAddress> pf)
+                    Function<Matcher, VisaAddress> pf)
         {
             pattern = p;
             parseFunction = pf;
         }
     }
 
-    private PatternParseFunction[] ppf =
-    {
-        new PatternParseFunction(VisaAddressPatterns.GPIB_INSTR_PATTERN,
-                (pattern) -> {
-
-
-                })
-    };
 
     private final static String[] ADDRESS_PATTERNS =
     {
@@ -187,9 +354,9 @@ class GpibVisaAddressParser implements IVisaAddressParser
     public VisaAddress parse(String address)
     {
 
-    }
+    }*/
 
-    @Override
+/*    @Override
     public boolean tryParse(String address, VisaAddress visaAddress)
     {
         for (String pattern : ADDRESS_PATTERNS)
@@ -204,9 +371,9 @@ class GpibVisaAddressParser implements IVisaAddressParser
         }
 
         return false;
-    }
+    }*/
 
-    private boolean extractAddressFields(Matcher matches)
+/*    private boolean extractAddressFields(Matcher matches)
     {
         Arrays.stream(ADDRESS_PATTERNS).filter((pattern)-> "".matches(pattern)).map();
 
@@ -219,7 +386,7 @@ class GpibVisaAddressParser implements IVisaAddressParser
         return true;
     }
 }
-
+*/
 enum VisaInterfaceType
 {
     VXI,
